e84c6746ec33284f8e57529b5d00dff5c151fbd6
giblets2570 <tomkeohanemurray@hotmail.com>
2014-12-05 15:54:02 +0000

    Update geocode.php

--- geocode.php
+++ geocode.php
@@ -8,8 +8,6 @@
 </head>
 <body>
  
-
-
 <?php
 if($_POST){
  
@@ -22,14 +20,15 @@ if($_POST){
         $latitude = $data_arr[0];
         $longitude = $data_arr[1];
         $formatted_address = $data_arr[2];
-                     
-    ?>
+    }   
+    
+?>
  
     <!-- JavaScript to show google map -->
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>    
     
  
-    <?php
+<?php
  
     // if unable to geocode the address
     }else{
@@ -49,56 +48,48 @@ if($_POST){
  
 // function to geocode address, it will return false if unable to geocode address
 function geocode($address){
- 
 
-
-
-$urlofaddress = urlencode("shoreditch.json");
-$resp_add_json = file_get_contents($urlofaddress);
-$resp_add = json_decode($resp_add_json,true);
-
-
-echo "Nim was here $resp_add";
-echo "<table border=1><tr><td><b>name</b></td><td><b>address</b></td><td><b>coords</b></td></tr>";
-
-if($resp_add['status']='OK'){
-for ($x = 0; $x <= 10; $x++) {
-
-
-
-   $addofpub = $resp_add['data'][$x]['address'][0];
-$nameofpub = $resp_add['data'][$x]['name'][0];    
- 
+    $urlofaddress = urlencode("shoreditch.json");
+    $resp_add_json = file_get_contents($urlofaddress);
+    $resp_add = json_decode($resp_add_json,true);
     
-// url encode the address
-$theadd = $addofpub;
-    $addofpub = urlencode($addofpub);
-     
-    // google map geocode api url
-    $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address={$addofpub}";
-
-    // get the json response
-    $resp_json = file_get_contents($url);
-     
-    // decode the json
-    $resp = json_decode($resp_json, true);
-
-
- 
-    // response status will be 'OK', if able to geocode given address 
-    if($resp['status']='OK'){
- 
-        // get the important data
-        $lati = $resp['results'][0]['geometry']['location']['lat'];
-        $longi = $resp['results'][0]['geometry']['location']['lng'];
-        $formatted_address = $resp['results'][0]['formatted_address'];
     
-      echo "<tr><td>  $nameofpub </td> <td> $theadd </td><td> ($lati , $longi) </td> </tr>";   
+    echo "Nim was here $resp_add";
+    echo "<table border=1><tr><td><b>name</b></td><td><b>address</b></td><td><b>coords</b></td></tr>";
+    
+    if($resp_add['status']='OK'){
+        for ($x = 0; $x <= 10; $x++) {
+    
+            $addofpub = $resp_add['data'][$x]['address'][0];
+            $nameofpub = $resp_add['data'][$x]['name'][0];    
+     
+        
+            // url encode the address
+            $theadd = $addofpub;
+            $addofpub = urlencode($addofpub);
+         
+            // google map geocode api url
+            $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address={$addofpub}";
+    
+            // get the json response
+            $resp_json = file_get_contents($url);
+         
+            // decode the json
+            $resp = json_decode($resp_json, true);
+            
+            // response status will be 'OK', if able to geocode given address 
+            if($resp['status']='OK'){
+     
+                // get the important data
+                $lati = $resp['results'][0]['geometry']['location']['lat'];
+                $longi = $resp['results'][0]['geometry']['location']['lng'];
+                $formatted_address = $resp['results'][0]['formatted_address'];
+            
+                echo "<tr><td>  $nameofpub </td> <td> $theadd </td><td> ($lati , $longi) </td> </tr>";   
+            }
+        } 
+        echo "</table>";
     }
-} 
-    echo "</table>";
-}
-
 
     else{
         return false;
