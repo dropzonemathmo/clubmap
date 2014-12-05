da8a08e75ac90beb195883b5aa3617a96a933785
Nim <nimsukumar@gmail.com>
2014-12-05 16:09:44 +0000

    updated google map javascipt

--- geocode.php
+++ geocode.php
@@ -3,7 +3,26 @@
 <head>
     
     <title>Location Accumulator</title>
-       
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script>
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
  
 </head>
 <body>
@@ -43,7 +62,7 @@ if($_POST){
     <input type='text' name='address' placeholder='Enter any address here' />
     <input type='submit' value='Geocode!' />
 </form>
- 
+ <div id="map-canvas"></div>
 <?php
  
 // function to geocode address, it will return false if unable to geocode address
