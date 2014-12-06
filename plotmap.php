a26f44d3f6e96367b658c2e7b4620aec973add03
giblets2570 <tomkeohanemurray@hotmail.com>
2014-12-06 01:48:23 +0000

    updated to show circles based on num reviews

--- plotmap.php
+++ plotmap.php
@@ -14,15 +14,14 @@
     
     <?php
         function geocode() {
-            $urlofaddress = urlencode("shoreditch.json");
+            $urlofaddress = urlencode("clubdata.json");
             $resp_add_json = file_get_contents($urlofaddress);
             $resp_add = json_decode($resp_add_json,true);
 
-            if($resp_add['status']='OK'){
                 for ($x = 0; $x <= 10; $x++) {
 
-                    $addofpub = $resp_add['data'][$x]['address'][0];
-                    $nameofpub = $resp_add['data'][$x]['name'][0];    
+                    $addofpub = $resp_add['results'][$x]['address'];
+                    $nameofpub = $resp_add['results'][$x]['name'];    
  
     
                     // url encode the address
@@ -45,20 +44,55 @@
                         $lati = $resp['results'][0]['geometry']['location']['lat'];
                         $longi = $resp['results'][0]['geometry']['location']['lng'];
                         $formatted_address = $resp['results'][0]['formatted_address'];
-                        if(strlen($long1 == 0)){
+                        
+                        if(gettype($lati) != "NULL"){
                             echo "$lati,$longi,";  
-                        }
-                        else{
-                            echo "0,0,";
                         }
                     }
                 }
-         
-            }
+    
+        } 
+    ?>
+    
+    
+    <?php
+        function reviews() {
+            $urlofaddress = urlencode("clubdata.json");
+            $resp_add_json = file_get_contents($urlofaddress);
+            $resp_add = json_decode($resp_add_json,true);
 
-            else{
-                return false;
-            }     
+                for ($x = 0; $x <= 10; $x++) {
+
+                    $addofpub = $resp_add['results'][$x]['address'];
+                    $nameofpub = $resp_add['results'][$x]['name'];    
+                    $noreviews = $resp_add['results'][$x]['no_reviews'];
+    
+                    // url encode the address
+                    $theadd = $addofpub;
+                    $addofpub = urlencode($addofpub);
+     
+                    // google map geocode api url
+                    $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address={$addofpub}";
+       
+                     // get the json response
+                    $resp_json = file_get_contents($url);
+     
+                    // decode the json
+                    $resp = json_decode($resp_json, true);
+
+                    // response status will be 'OK', if able to geocode given address 
+                   if($resp['status']='OK'){
+ 
+                       // get the important data
+                        $lati = $resp['results'][0]['geometry']['location']['lat'];
+                        $longi = $resp['results'][0]['geometry']['location']['lng'];
+                        $formatted_address = $resp['results'][0]['formatted_address'];
+                        
+                        //if(gettype($lati) != "NULL"){
+                            echo "$noreviews,";  
+                        //}
+                    }
+                }   
         } 
     ?>
     
@@ -70,26 +104,55 @@
         var markers = [];
 
         function initialize() {
-            var london = new google.maps.LatLng(51.5072, 0.1275);
+            var london = new google.maps.LatLng(51.5238121,-0.0744264);
             var mapOptions = {
-                zoom: 12,
+                zoom: 20,
                 center: london,
                 mapTypeId: google.maps.MapTypeId.TERRAIN
             };
             map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
             var coords = [<?php
-                geocode();  
+                echo geocode(); 
+            ?>];
+            
+            var no_reviews = [<?php
+                echo reviews();
             ?>];
             
             
+            /*for(var i = 0; i < coords.length/2; i++){
+                document.write(coords[2*i] + " " + coords[2*i + 1] + " " + no_reviews[i] + "<br>");
+            
+            }*/
+            
             for(var i = 0; i < coords.length/2; i++){
                 var pubLocation = new google.maps.LatLng(coords[2*i],coords[2*i + 1]);
-                addMarker(pubLocation,"name");
-                
+                //addMarker(pubLocation,"name");
+                var colour = getRandomColor();
+                var populationOptions = {  
+      				strokeColor: colour,
+      				strokeOpacity: 0.8,
+      				strokeWeight: 2,
+      				fillColor: colour,
+      				fillOpacity: 0.35,
+      				map: map,
+      				center: pubLocation,
+      				radius: Math.sqrt(parseFloat(no_reviews[i]))*25
+    			};
+                cityCircle = new google.maps.Circle(populationOptions);
             }
             
 
+        }
+        
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++ ) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
         }
         
 
